648d1ce5b139259c40d954b001cb2507c012170b
Ivan Rodrigues <ivan-guita@hotmail.com>
2025-08-29 11:20:06 -0300

    ver o bug do login admin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
@@ -13,5 +14,7 @@ Route::middleware('guest')->group(function () {
 Route::middleware('auth')->group(function () {
 
    Route::redirect('/','/home');
+   Route::get('/home',[MainController::class,'index'])->name('home');
+   Route::get('/logout',[AuthController::class,'logout'])->name('logout');
 
 });
